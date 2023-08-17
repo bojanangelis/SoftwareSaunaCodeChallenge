bba1b1585156c3b1e0538784c5b48172d94f8cef
bojanangelis <bojanangeleski98@gmail.com>
2023-08-18 00:27:26 +0200

    adding errors on tests

--- tests/PathWalker.test.ts
+++ tests/PathWalker.test.ts
@@ -64,3 +64,66 @@ describe('Do not collect a letter from the same location twice', () => {
     expect(result.path).toBe('@-G-O-+|+-+|O||+-O-N-+|I|+-+|+-I-+|ES|x')
   })
 })
+
+// describe('Keep direction, even in a compact space', () => {
+//   it('should collect letters and determine path for Keep direction, even in a compact space', () => {
+//     const map = [
+//       [' ', '+', '-', 'L', '-', '+', ' ', ' '],
+//       [' ', '|', ' ', ' ', '+', 'A', '-', '+'],
+//       ['@', 'B', '+', ' ', '+', '+', ' ', 'H'],
+//       [' ', '+', '+', ' ', ' ', ' ', ' ', 'X'],
+//     ]
+//     const result = PathWalker(map)
+//     expect(result.letters).toBe('BLAH')
+//     expect(result.path).toBe('@B+++B|+-L-+A+++A-+HX')
+//   })
+// })
+
+describe('Ignore stuff after end of path', () => {
+  it('should collect letters and determine path for Ignore stuff after end of path  ', () => {
+    const map = [
+      ['@', '-', 'A', '-', '-', '+', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', '+', '-', 'B', '-', '-', 'x', '-', 'C', '-', '-', 'D'],
+    ]
+    const result = PathWalker(map)
+    expect(result.letters).toBe('AB')
+    expect(result.path).toBe('@-A--+|+-B--x')
+  })
+})
+
+describe('Missing start character', () => {
+  const containsAtSymbol = (map: string[][]): boolean => {
+    return map.some((row) => row.includes('@'))
+  }
+  it('ERROR --> Missing start character', () => {
+    const map = [
+      [' ', '-', '-', '-', 'A', '-', '-', '-', '+'],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'],
+      ['x', '-', 'B', '-', '+', ' ', ' ', ' ', 'C'],
+      [' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|'],
+      [' ', ' ', ' ', ' ', '+', '-', '-', '-', '+'],
+    ]
+    if (!containsAtSymbol(map)) {
+      throw new Error('Test map is missing the "@" character.')
+    }
+  })
+})
+
+describe('Missing end character', () => {
+  const containsEndSymbol = (map: string[][]): boolean => {
+    return map.some((row) => row.includes('x') || row.includes('X'))
+  }
+  it('ERROR --> Missing end character', () => {
+    const map = [
+      [' ', '-', '-', '-', 'A', '-', '-', '-', '+'],
+      ['@', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'],
+      [' ', '-', 'B', '-', '+', ' ', ' ', ' ', 'C'],
+      [' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|'],
+      [' ', ' ', ' ', ' ', '+', '-', '-', '-', '+'],
+    ]
+    if (!containsEndSymbol(map)) {
+      throw new Error('Test map is missing the "x" character.')
+    }
+  })
+})
--- utils/FindInitialDirection.ts
+++ utils/FindInitialDirection.ts
@@ -1,11 +1,14 @@
 import { Direction } from '../types/Direction'
 import { Position } from '../types/Position'
+import { isLetter } from './IsLetter'
 
-export const findInitialDirection = (map: string[][], startPos: Position): Direction => {
-  const { x, y } = startPos
-  if (map[y][x + 1] === '-') return 'RIGHT'
-  if (map[y][x - 1] === '-') return 'LEFT'
-  if (map[y + 1] && map[y + 1][x] === '|') return 'DOWN'
-  if (map[y - 1] && map[y - 1][x] === '|') return 'UP'
+export const findInitialDirection = (map: string[][], position: Position): Direction => {
+  const { x, y } = position
+
+  if (map[y][x + 1] === '-' || isLetter(map[y][x + 1])) return 'RIGHT'
+  if (map[y][x - 1] === '-' || isLetter(map[y][x - 1])) return 'LEFT'
+  if (map[y + 1] && (map[y + 1][x] === '|' || isLetter(map[y + 1][x]))) return 'DOWN'
+  if (map[y - 1] && (map[y - 1][x] === '|' || isLetter(map[y - 1][x]))) return 'UP'
+
   throw new Error('Invalid map: No clear initial direction')
 }
